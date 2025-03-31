9c01e9aea4519ad0f0ed9348f7bf13bf926951c4
Saterz_ <136126641+Saterz@users.noreply.github.com>
2025-03-31 09:29:22 +0100

    Remove unused theme switcher code and improve footer styles

--- script.js
+++ script.js
@@ -1,61 +1,6 @@
 console.log('Pk t\'es dans la console ?');
 console.log('Tqt pas, y\'a pas d\'erreur');
 
-// const themeSwitcher = document.querySelector('i');
-
-// themeSwitcher.addEventListener('click', () => {
-//     themeSwitcher.classList.toggle('fa-moon');
-//     changeTheme();
-// });
-
-// function changeTheme() {
-//     const body = document.querySelector('body');
-//     const titles = document.querySelectorAll('.titles');
-//     const nav = document.querySelector('nav');
-//     const footer = document.querySelector('footer');
-//     const cards = document.querySelectorAll('.card');
-//     const allTextElements = document.querySelectorAll('body *');
-
-//     if (themeSwitcher.classList.contains('fa-moon')) {
-//         // Dark mode
-//         body.style.backgroundColor = '#121212'; // dark background
-//         titles.forEach(title => {
-//             title.style.color = '#ffffff';
-//         });
-//         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-//         nav.style.color = '#ffffff';
-//         footer.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-//         footer.style.color = '#ffffff';
-//         cards.forEach(card => {
-//             card.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-//             card.style.color = '#ffffff';
-//         });
-//         // Modify all text color to white
-//         allTextElements.forEach(element => {
-//             element.style.color = '#ffffff';
-//         });
-//     } else {
-//         // Light mode
-//         body.style.backgroundColor = 'white';
-//         titles.forEach(title => {
-//             title.style.color = 'black';
-//         });
-//         nav.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
-//         nav.style.color = 'black';
-//         footer.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
-//         footer.style.color = 'black';
-//         cards.forEach(card => {
-//             card.style.backgroundColor = 'rgba(255, 255, 255, 0.2)';
-//             card.style.color = 'black';
-//         });
-//         // Modify all text color to black
-//         allTextElements.forEach(element => {
-//             element.style.color = 'black';
-//         });
-//     }
-// }
-
-
 // Menu button
 const menuButton = document.querySelector('.button.menu');
 const menuIcon = document.querySelector('.fa-bars');
